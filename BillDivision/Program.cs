41a6ec8ab7727413483843fa973e11db4b4260d4
Duska <Duska@DESKTOP-O3AR66J>
2022-06-26 14:51:25 +0200

    Renaming variables

--- BillDivision/Program.cs
+++ BillDivision/Program.cs
@@ -9,13 +9,13 @@ namespace BillDivision
         {
             Console.WriteLine("Enter meals prices");
             var arr = Console.ReadLine().Split(' ');
-            var arrayOfPrices = Array.ConvertAll(arr, int.Parse);
+            var bill = Array.ConvertAll(arr, int.Parse);
             Console.WriteLine("Enter the index of meal that Anna did not eat");
-            var indexOfItemThatAnnaDidNotEat = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the total amount");
-            var amountCalculatedByTom = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(DivisionLogic.BonAppetit(arrayOfPrices, indexOfItemThatAnnaDidNotEat, amountCalculatedByTom));
+            Console.WriteLine(DivisionLogic.BonAppetit(bill, k, b));
         }
     }
 }
